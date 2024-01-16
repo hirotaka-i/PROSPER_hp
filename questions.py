7af056ab618e94b07c52d5c51341c63d74857fc3
hirotaka-i <hirotaka.i@gmail.com>
2024-01-16 09:38:50 -0500

    track the questionnaire data

--- questions.py
+++ questions.py
@@ -39,49 +39,64 @@ def app():
     else:
         user = auth.get_user(st.session_state.username)
         st.session_state.custom_claims=user.custom_claims
-        st.markdown(st.session_state.custom_claims)
         
-
-        if st.button("Reset Custom Claims"):
-            reset_custom_claims(st.session_state.username)
-            st.success("Custom claims reset!")
+        
+        # Check command (comment out for production)
+        # st.markdown(st.session_state.custom_claims)
+        # if st.button("Reset Custom Claims"):
+        #     reset_custom_claims(st.session_state.username)
+        #     st.success("Custom claims reset!")
         
         st.warning('アンケートには予めメンバーIDが入力されています。こちらは変更せずに次に進んでください\n')
         
-        st.markdown('\n')
-        st.markdown(f'[基本情報アンケート](https://docs.google.com/forms/d/e/1FAIpQLSeczVrafMvPsGrsqQsNY8U8jBH5U2JUcB5Uf_Y2hlPe2MmZeg/viewform?usp=pp_url&entry.1850808469={st.session_state.username})')
-        st.markdown('\n')
-        st.markdown(f'[経過情報アンケート](https://docs.google.com/forms/d/e/1FAIpQLSdAG4PcOxFNITy4PbimVyg9Z0SjYqhqssQop0FvzHVJBOev_g/viewform?usp=pp_url&entry.2128319109={st.session_state.username})')
-        st.markdown('\n')
-        st.markdown(f'[生活アンケート１](https://docs.google.com/forms/d/e/1FAIpQLScue4b3DzDJ--YDEPXY_z7mK4oojFyBy1LgbwLStrDMz6sDfA/viewform?usp=pp_url&entry.1185128633={st.session_state.username})')
-        st.markdown('\n')
-        st.markdown(f'[生活アンケート２](https://docs.google.com/forms/d/e/1FAIpQLSc0nmQY5ruw7FKNKcrNOWPff4LniJrz2tCm-gduVi9Bj2PToA/viewform?usp=pp_url&entry.692875108={st.session_state.username})')
-        st.markdown('\n')
-        st.markdown(f'[test](https://docs.google.com/forms/d/e/1FAIpQLSf7ycjTwE2sbV-rXqCGoyfzQHMTf69ms8B7SEGGnV2jn_1cYA/viewform?usp=pp_url&entry.2056512396={st.session_state.username})')
-        
         test_one='このアンケートは最初に一度だけご回答ください'
         test_rep='このアンケートは何度回答していただいても構いません。'
-        
-        ###
-        st.markdown('#### テストアンケート')
-        form00='Test'
-        link=f'https://docs.google.com/forms/d/e/1FAIpQLSf7ycjTwE2sbV-rXqCGoyfzQHMTf69ms8B7SEGGnV2jn_1cYA/viewform?usp=pp_url&entry.2056512396={st.session_state.username}'
-        if form00 in st.session_state.custom_claims.keys():
-            st.markdown(f'[最後に回答した日] {st.session_state.custom_claims[form00]}')
-            st.markdown(test_rep)
-        else:
-            st.markdown(f'まだ回答していません')
-        if st.button('テストをはじめる'):
-            handle_form_button_click(st.session_state.username, form00, link)   
 
         ###
         st.markdown('#### 基本情報アンケート')
         form01='Basic'
-        link=f'https://docs.google.com/forms/d/e/1FAIpQLSeczVrafMvPsGrsqQsNY8U8jBH5U2JUcB5Uf_Y2hlPe2MmZeg/viewform?usp=pp_url&entry.1850808469={st.session_state.username}'
+        link01=f'https://docs.google.com/forms/d/e/1FAIpQLSeczVrafMvPsGrsqQsNY8U8jBH5U2JUcB5Uf_Y2hlPe2MmZeg/viewform?usp=pp_url&entry.1850808469={st.session_state.username}'
         if form01 in st.session_state.custom_claims.keys():
             st.markdown(f'[最後に回答した日時] {st.session_state.custom_claims[form01]}')
             st.markdown(test_one)
         else:
             st.markdown(f'[最後に回答した日時] まだ回答していません')
             if st.button('基本情報アンケートをはじめる'):
-                handle_form_button_click(st.session_state.username, form01, link)
+                handle_form_button_click(st.session_state.username, form01, link01)
+
+        ###
+        st.markdown('#### 経過情報アンケート')
+        form_keika='keika'
+        link_keika=f'https://docs.google.com/forms/d/e/1FAIpQLSdAG4PcOxFNITy4PbimVyg9Z0SjYqhqssQop0FvzHVJBOev_g/viewform?usp=pp_url&entry.2128319109={st.session_state.username})'
+        if form_keika in st.session_state.custom_claims.keys():
+            st.markdown(f'[最後に回答した日時] {st.session_state.custom_claims[form_keika]}')
+            st.markdown(test_rep)
+        else:
+            st.markdown(f'[最後に回答した日時] まだ回答していません')
+        if st.button('経過情報アンケートをはじめる'):
+            handle_form_button_click(st.session_state.username, form_keika, link_keika)
+
+
+        ###
+        st.markdown('#### 生活アンケート１')
+        form_life1='mds_updrs1'
+        link_life1=f'https://docs.google.com/forms/d/e/1FAIpQLScue4b3DzDJ--YDEPXY_z7mK4oojFyBy1LgbwLStrDMz6sDfA/viewform?usp=pp_url&entry.1185128633={st.session_state.username})'
+        if form_life1 in st.session_state.custom_claims.keys():
+            st.markdown(f'[最後に回答した日時] {st.session_state.custom_claims[form_life1]}')
+            st.markdown(test_rep)
+        else:
+            st.markdown(f'[最後に回答した日時] まだ回答していません')
+        if st.button('生活アンケート１をはじめる'):
+            handle_form_button_click(st.session_state.username, form_life1, link_life1)
+
+        ###
+        st.markdown('#### 生活アンケート２')
+        form_life2='mds_updrs2'
+        link_life2=f'https://docs.google.com/forms/d/e/1FAIpQLSc0nmQY5ruw7FKNKcrNOWPff4LniJrz2tCm-gduVi9Bj2PToA/viewform?usp=pp_url&entry.692875108={st.session_state.username})'
+        if form_life2 in st.session_state.custom_claims.keys():
+            st.markdown(f'[最後に回答した日時] {st.session_state.custom_claims[form_life2]}')
+            st.markdown(test_rep)
+        else:
+            st.markdown(f'[最後に回答した日時] まだ回答していません')
+        if st.button('生活アンケート２をはじめる'):
+            handle_form_button_click(st.session_state.username, form_life2, link_life2)
